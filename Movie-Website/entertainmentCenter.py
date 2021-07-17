b3f6410e679a97d729d7cd94b54314d9bb625e3c
Arjun Jadhav <63765264+arjundhav@users.noreply.github.com>
2021-07-17 08:01:10 +0530

    Update entertainmentCenter.py

--- Movie-Website/entertainmentCenter.py
+++ Movie-Website/entertainmentCenter.py
@@ -1,3 +1,4 @@
+#import media.py file/fresh_tomatoes
 import media
 import fresh_tomatoes
 
@@ -6,6 +7,7 @@ if __name__ == '__main__':
 	toystory = media.Movie("Toy Story", "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450", "Woody (Tom Hanks), a good-hearted cowboy doll who belongs to a young boy named Andy (John Morris), sees his position as Andy's favorite toy jeopardized when his parents buy him a Buzz Lightyear (Tim Allen) action figure.", "https://www.youtube.com/watch?v=gFTXLCCPPCs")
 	saveTheLastDance = media.Movie("Save the Last Dance", "http://www.gstatic.com/tv/thumb/movieposters/26927/p26927_p_v8_aa.jpg", "Sara (Julia Stiles) is moved from a small Midwestern town to the south side of Chicago when her mother dies in a car accident, and must live with her father.", "https://www.youtube.com/watch?v=dhAxDeSxYbQ")
 
+#puts movies in list to pass to open_movies_page function from fresh_tomatoes
 	movies = [avatar, toystory, saveTheLastDance]
 
 	fresh_tomatoes.open_movies_page(movies)
